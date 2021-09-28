61153656d4bb989b1e6d3857ee89be66a65eb889
Inigo Ramli <inigo.ramli@ui.ac.id>
2021-09-28 19:37:08 +0700

    Create a copy of LM head for KEPLER

--- src/transformers/models/kepler/modeling_kepler.py
+++ src/transformers/models/kepler/modeling_kepler.py
@@ -10,7 +10,7 @@ from torch import nn
 from torch.nn import BCEWithLogitsLoss, CrossEntropyLoss, MSELoss
 import torch.nn.functional as F
 
-from ...activations import ACT2FN, get_activation
+from ...activations import gelu
 from ...file_utils import (
     ModelOutput,
     add_code_sample_docstrings,
@@ -73,7 +73,77 @@ class KeplerForPreTrainingOutput(ModelOutput):
     attentions: Optional[Tuple[torch.FloatTensor]] = None
 
 
-class KeplerKnowledgeEmbeddingPredictions(nn.Module):
+class KeplerLMHead(PreTrainedModel):
+    def __init__(self, config, encoder):
+        super().__init__(config)
+
+        self.encoder = encoder
+        self.vocab_transform = nn.Linear(config.dim, config.dim)
+        self.vocab_layer_norm = nn.LayerNorm(config.dim, eps=1e-12)
+        self.vocab_projector = nn.Linear(config.dim, config.vocab_size)
+
+        self.init_weights()
+
+        self.mlm_loss_fct = nn.CrossEntropyLoss()
+
+    def get_position_embeddings(self) -> nn.Embedding:
+        return self.encoder.get_position_embeddings()
+
+    def resize_position_embeddings(self, new_num_position_embeddings: int):
+        self.encoder.resize_position_embeddings(new_num_position_embeddings)
+
+    def get_output_embeddings(self):
+        return self.vocab_projector
+
+    def set_output_embeddings(self, new_embeddings):
+        self.vocab_projector = new_embeddings
+
+    def forward(
+        self,
+        input_ids=None,
+        attention_mask=None,
+        head_mask=None,
+        inputs_embeds=None,
+        labels=None,
+        output_attentions=None,
+        output_hidden_states=None,
+        return_dict=None,
+    ):
+        r"""
+        labels (:obj:`torch.LongTensor` of shape :obj:`(batch_size, sequence_length)`, `optional`):
+            Labels for computing the masked language modeling loss. Indices should be in ``[-100, 0, ...,
+            config.vocab_size]`` (see ``input_ids`` docstring) Tokens with indices set to ``-100`` are ignored
+            (masked), the loss is only computed for the tokens with labels in ``[0, ..., config.vocab_size]``.
+        """
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+
+        encoder_output = self.encoder(
+            input_ids=input_ids,
+            attention_mask=attention_mask,
+            head_mask=head_mask,
+            inputs_embeds=inputs_embeds,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
+        )
+        hidden_states = encoder_output[0]  # (bs, seq_length, dim)
+        prediction_logits = self.vocab_transform(hidden_states)  # (bs, seq_length, dim)
+        prediction_logits = gelu(prediction_logits)  # (bs, seq_length, dim)
+        prediction_logits = self.vocab_layer_norm(prediction_logits)  # (bs, seq_length, dim)
+        prediction_logits = self.vocab_projector(prediction_logits)  # (bs, seq_length, vocab_size)
+
+        mlm_loss = None
+        if labels is not None:
+            mlm_loss = self.mlm_loss_fct(prediction_logits.view(-1, prediction_logits.size(-1)), labels.view(-1))
+
+        if not return_dict:
+            output = (prediction_logits,) + encoder_output[1:]
+            return ((mlm_loss,) + output) if mlm_loss is not None else output
+
+        return mlm_loss, prediction_logits, encoder_output.hidden_states, encoder_output.attentions
+
+
+class KeplerKEHead(nn.Module):
     def __init__(self, config, encoder):
         super().__init__()
 
@@ -164,12 +234,12 @@ class KeplerModel(PreTrainedModel):
     config_class = KeplerConfig
     base_model_prefix = "kepler"
 
-    def __init__(self, config, encoder, mlm_head):
+    def __init__(self, config, encoder):
         super().__init__(config)
 
         self.encoder = encoder
-        self.mlm_head = mlm_head
-        self.ke_head = KeplerKnowledgeEmbeddingPredictions(config, self.encoder)
+        self.mlm_head = KeplerLMHead(config, self.encoder)
+        self.ke_head = KeplerKEHead(config, self.encoder)
         self.config = config
 
     def _init_weights(self, module):
@@ -192,26 +262,26 @@ class KeplerModel(PreTrainedModel):
     ):
         return_dict = return_dict if return_dict is not None else self.config.use_return_dict
 
-        mlm_output = self.mlm_head(**mlm_data)
+        mlm_loss, mlm_logits, mlm_hidden_states, mlm_attentions = self.mlm_head(**mlm_data)
 
         pScore, nScore, ke_loss = None, None, None
         if ke_data is not None:
             pScore, nScore, ke_loss = self.ke_head(**ke_data)
             
         loss = None
-        if ke_loss is not None and mlm_output.loss is not None:
-            loss = mlm_output.loss + ke_loss
+        if ke_loss is not None and mlm_loss is not None:
+            loss = mlm_loss + ke_loss
 
         if not return_dict:
-            output = (mlm_output.logits,) + mlm_output.hidden_states[1:]
+            output = (mlm_logits, pScore, nScore) + mlm_hidden_states[1:]
             return ((loss,) + output) if loss is not None else output
 
         return KeplerForPreTrainingOutput(
             loss=loss,
-            logits=mlm_output.logits,
+            logits=mlm_logits,
             pScore=pScore,
             nScore=nScore,
-            hidden_states=mlm_output.hidden_states,
-            attentions=mlm_output.attentions,
+            hidden_states=mlm_hidden_states,
+            attentions=mlm_attentions,
         )
     
